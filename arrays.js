2515b738d0808529617470dad9e4d38dd57b2288
Siza-Mpafa030628 <sizampafa972@gmail.com>
2025-10-29 21:50:01 +0200

    arrays revision/exercise

---
+++ arrays.js
@@ -0,0 +1,21 @@
+// TODO: Create an empty array called colors
+// TODO: Add three colors to the array using push()
+// TODO: Create another array called numbers with five numbers
+// TODO: Remove the last color from the colors array
+// TODO: Add a new color to the beginning of the colors array
+// TODO: Print the length of both arrays
+
+let colors = []
+colors.push("wisteria")
+colors.push("violet")
+colors.push("teal")
+console.log(colors);
+console.log(colors.length);
+const removedColor = colors.pop()
+console.log(removedColor);
+console.log(colors);
+console.log(colors.length);
+colors.unshift("indigo")
+console.log(colors);
+console.log(colors.length);
+
