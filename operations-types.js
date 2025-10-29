288ac836053579602b2834b0f52086fa7f1ef8a0
Siza-Mpafa030628 <sizampafa972@gmail.com>
2025-10-29 21:14:23 +0200

    operations revision/exercise 2

---
+++ operations-types.js
@@ -0,0 +1,15 @@
+// TODO: Create a variable called number1 and store any number in it
+// TODO: Create a variable called number2 and store a different number in it
+// TODO: Convert number1 to a string and store it in a new variable
+// TODO: Add number1 and number2 and store the result in a variable called sum
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+// TODO: Print all results to the console
+
+const number1 = -1
+const number2 = 1
+const to_s = number1.toString()
+console.log(typeof to_s);
+const sum = number1 + number2
+console.log(sum);
+const product = number1 * number2
+console.log(product);
