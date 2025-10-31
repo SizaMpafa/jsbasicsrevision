72250432c387b223e66c8297ec5fdfcc253d8882
SizaMpafa <sizampafa972@gmail.com>
2025-10-31 10:25:51 +0200

    Conditional Staments revision

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+let score = 0
+passingGrade = 60
+
+ if (score >= passingGrade) {
+    grade = `The passing grade is ${passingGrade} and you scored ${score}. You did TREMENDOUSLY well, that's a PASS! `
+} else {
+    grade = `The passing grade is ${passingGrade} and you scored ${score}. Are you proud of yourself? FAILED!!!! `
+    
+ }
+
+ console.log(grade);
+ 
